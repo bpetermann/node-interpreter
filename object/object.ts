7d0e9f0aa001780267afb78f163b0639b9d1a73c
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-11-24 10:48:43 +0100

    Add object types

---
+++ object/object.ts
@@ -0,0 +1,44 @@
+enum ObjectType {
+  INTEGER_OBJ = 'INTEGER',
+  BOOLEAN_OBJ = 'BOOLEAN',
+  NULL_OBJ = 'NULL',
+}
+
+interface Object {
+  type: () => ObjectType;
+  inspect: () => string;
+}
+
+class IntegerObject implements Object {
+  value: number;
+
+  type(): ObjectType {
+    return ObjectType.INTEGER_OBJ;
+  }
+
+  inspect(): string {
+    return `${this.value}`;
+  }
+}
+
+class BooleanObject implements Object {
+  value: boolean;
+
+  type(): ObjectType {
+    return ObjectType.BOOLEAN_OBJ;
+  }
+
+  inspect(): string {
+    return `${this.value}`;
+  }
+}
+
+class NullObject implements Object {
+  type(): ObjectType {
+    return ObjectType.NULL_OBJ;
+  }
+
+  inspect(): string {
+    return `null`;
+  }
+}
