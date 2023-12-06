6aa7d351f8a15bae8ec92a9f7ba7ec3e50f9e244
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-12-06 22:50:22 +0100

    Change rl to repl property

--- repl/Repl.ts
+++ repl/Repl.ts
@@ -5,12 +5,17 @@ import readline from 'readline';
 import { Eval } from '../eval';
 import colors from 'colors';
 export default class Repl {
-  private _eval: Eval;
+  rl: readline.Interface;
+  private eval: Eval;
   env: Environment;
 
   constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
     this.env = new Environment({});
-    this._eval = new Eval();
+    this.eval = new Eval();
   }
 
   private print(line: string) {
@@ -18,18 +23,21 @@ export default class Repl {
     const program = parser.parse();
 
     if (parser.errors.length) {
-      console.log(parser.errors);
+      parser.errors.forEach((err) => {
+        console.log(colors.red(err));
+      });
       return;
     }
 
-    const evaluated = this._eval.evaluate(program, this.env);
-    evaluated.map((obj) => {
+    const evaluated = this.eval.evaluate(program, this.env);
+    evaluated.forEach((obj) => {
       console.log(obj ? obj.inspect() : colors.gray('undefined'));
     });
   }
 
   private processInput(input: string) {
     if (input.trim().toUpperCase() === TokenType.EOF) {
+      this.rl.close();
       process.exit(0);
     }
 
@@ -38,14 +46,8 @@ export default class Repl {
     this.start();
   }
 
-  async start() {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-    rl.question('>> ', (input) => {
-      rl.close();
+  start() {
+    this.rl.question('>> ', (input) => {
       this.processInput(input);
     });
   }
